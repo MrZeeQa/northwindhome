bb5e72ade95cb80a3caf93e416e235bd2b3ef16f
Nicolas Bouquiaux <nicolas.bouquiaux@jomasecundair.be>
2020-03-13 12:36:25 +0100

    een nieuwe aanpassing in een origibranch

---
+++ northwindhome/App_Code/testCS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for testCS
+/// </summary>
+public class testCS
+{
+    public testCS()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
